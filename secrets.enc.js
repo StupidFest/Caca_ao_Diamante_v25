6f6f9758881adac49c65954322173f25df33e6ab
StupidFest <feststupid@gmail.com>
2025-11-06 14:23:47 -0300

    Add files via upload

---
+++ secrets.enc.js
@@ -0,0 +1,11 @@
+/*
+ GENERATED secrets.enc.js - keep secrets.json private.
+ Do NOT store passphrase or secrets.json in the repo.
+ This file contains encrypted payload.
+*/
+
+const ENCRYPTED_PAYLOAD = {
+  "iv": "kLdlqb93L84NBUM+",
+  "ciphertext": "lQrn5dOE9XJ0dgv+0y5fF3zxunlp9xowHkYYrJ6ugCenn9Nds4s4Bu9ybK7S3aIh1ZmjdUJvnkBNMA+emqJRPEg7MmofIEekYLL68ZlT5m1Uape66ugsmklrg9ntHqkf08KcbEkZwdWNNRE2i8UAwXCgiGsPw0TbMV663QAVqZVdFVJ9SxX2zMu1j4d7PApGRaG2wnOgx+ge2DM=",
+  "tagLength": 128
+};
